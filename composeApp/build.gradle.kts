54e7ad8aeb00e7845666ae394aa503467fe74e2c
Sunil Kumar <sunil.dhiman90@gmail.com>
2024-05-25 09:59:32 +0530

    Upgrade to Kotlin 2.0 & Compose UI 1.6.10 and other required dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,6 +9,9 @@ plugins {
     kotlin("plugin.serialization") version "1.9.21" //decompose step2
 
     id("app.cash.sqldelight") version "2.0.1" //sqldelight step1
+
+    //for Kotlin 2.0
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
