df48b83c035c072aa6bec40a2e363d55c728514d
Sunil Kumar <sunil.dhiman90@gmail.com>
2025-05-27 16:09:03 +0530

    Enable edge to edge

--- composeApp/src/androidMain/kotlin/com/codingambitions/kmpapp2/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/codingambitions/kmpapp2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.codingambitions.kmpapp2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
@@ -43,6 +44,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             CompositionLocalProvider(
